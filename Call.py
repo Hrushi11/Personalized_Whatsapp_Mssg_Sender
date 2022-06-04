118698f1278ca8c2624d1a4d420d8dd794970b75
Hrushikesh <hrushiskachgunde@gmail.com>
2022-06-04 20:21:16 +0530

    add chnges

--- Call.py
+++ Call.py
@@ -11,10 +11,12 @@ min_ = extractor.now_min
 for i in range(extractor.contacts):
     # tag = " @" if names[i] != "" else ""
     tag = "" if names[i] == "" else " "
-    mssg = f"Hi{tag}{names[i]},\nThanks a lot for your well wishes on my birthday.\nRegards Hrushikesh"
+    mssg = f"Hi{tag}{names[i]},\nThanks a lot for your well wishes on my birthday."
     mssgs.append(mssg)
 
-for i in range(extractor.contacts):
+for i in range(64, extractor.contacts):
+    # min_ = 0 if min_ == 60 else min_
+    # hr = hr + 1 if min_ == 60 else hr
     pywhatkit.sendwhatmsg(ph_numbers[i], mssgs[i], time_hour=hr, time_min=min_)
     min_ += 1
 
